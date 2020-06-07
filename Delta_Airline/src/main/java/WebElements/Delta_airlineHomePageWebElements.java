7e2f024fa06ce3d69ff3fb48cb6aa25eb0a7d98c
Asif <asif.sirf@gmail.com>
2020-06-07 13:36:46 -0700

    Ebay Domain updated

---
+++ Delta_Airline/src/main/java/WebElements/Delta_airlineHomePageWebElements.java
@@ -0,0 +1,16 @@
+package WebElements;
+
+public class Delta_airlineHomePageWebElements {
+
+
+    public static final String handleCookiesWebElement = "/html/body/app-root/app-home/ngc-global-footer/footer/ngc-cookie-banner/div/div/div/div[2]/button";
+    public static final String carriersButtonWebElement = "Careers";
+    public static final String searchDeltaCarriersWebElement = "/html/body/div[3]/div/div[2]/main/div[2]/div/div/div/div[2]/div/div/div/div/div/div/a";
+    public static final String notificationsButtonWebElement = "/html/body/app-root/app-home/ngc-global-nav/header/div/nav/div/div[2]/ngc-notification/div/a";
+    public static final String firstNotificationWebElement = "/html/body/modal-container/div/div/ngc-flyout-feed/div/div/div/div/div/div/div[2]/div/tabset/div/tab[1]/ul/li[1]/a/h5";
+    public static final String flightStatusButtonWebElement = "FLIGHT STATUS";
+    public static final String searchBoxWebElement = "flightNo";
+    public static final String searchButtonWebElement = "btn-flight-sts-submit";
+    public static final String searchInputWebElement="/html/body/app-root/app-home/ngc-global-nav/header/div/nav/div/div[2]/ngc-search/div/a";
+    public static final String keyWordInputWebElement="#search_input";
+}
--- Delta_Airline/src/main/java/homepage/Delta_AirlineHomePage.java
+++ Delta_Airline/src/main/java/homepage/Delta_AirlineHomePage.java
@@ -1,4 +1,85 @@
 package homepage;
 
-public class Delta_AirlineHomePage {
+import common.WebAPI;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+import static WebElements.Delta_airlineHomePageWebElements.*;
+
+public class Delta_AirlineHomePage extends WebAPI {
+
+    @FindBy(how = How.XPATH, using = handleCookiesWebElement) public WebElement handleCookies;
+    @FindBy(how = How.LINK_TEXT,using = carriersButtonWebElement) public WebElement carriesButton;
+    @FindBy(how = How.XPATH, using = searchDeltaCarriersWebElement) public WebElement searchDeltaCarries;
+    @FindBy(how = How.XPATH, using = notificationsButtonWebElement) public WebElement notificationsButton;
+    @FindBy(how=How.XPATH,using = firstNotificationWebElement) public WebElement firstNotification;
+    @FindBy(how=How.LINK_TEXT, using= flightStatusButtonWebElement) public WebElement flightStatusButton;
+    @FindBy(how=How.ID, using= searchBoxWebElement) public WebElement searchBox;
+    @FindBy(how= How.ID, using= searchButtonWebElement) public WebElement searchButton;
+    @FindBy(how=How.XPATH, using= searchInputWebElement) public WebElement searchInput;
+    @FindBy(how=How.CSS, using= keyWordInputWebElement) public  WebElement keywordInput;
+
+
+
+
+    public void clickHandleCookies() {handleCookies.click(); }
+
+    public void clickCarriesButton() {
+        carriesButton.click();
+    }
+    public void clickSearchDeltaCarries() {
+        searchDeltaCarries.click();
+    }
+    public void clickNotificationButton(){ notificationsButton.click();}
+    public void clickFirstNotification(){ firstNotification.click();}
+    public void clickFlightStatusButton(){ flightStatusButton.click();}
+    public void enterKeyWord(String keyWord){searchBox.sendKeys(keyWord);}
+    public void clickSearchButton(){ searchButton.click();}
+    public void clickSearchInput(){ searchInput.click();}
+    public void inputKeyWord(String keyword){ keywordInput.sendKeys(keyword);}
+
+    public void deltaCarriersSearchPage() {
+        clickHandleCookies();
+        clickCarriesButton();
+        clickSearchDeltaCarries();
+    }
+
+    public void checkNotifications(){
+        clickHandleCookies();
+        clickNotificationButton();
+        clickFirstNotification();
+    }
+
+
+   public void checkFlightStatus(){
+       clickHandleCookies();
+       clickFlightStatusButton();
+       enterKeyWord("1740");
+       clickSearchButton();
+   }
+
+   public void searchButtonOptions(){
+       clickHandleCookies();
+       clickSearchInput();
+       inputKeyWord("check-in");
+
+   }
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
