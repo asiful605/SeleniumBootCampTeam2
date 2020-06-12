8d80bdfa496e51871097482e3a758b66277bf000
Asif <asif.sirf@gmail.com>
2020-06-12 15:42:32 -0700

    added more test cases

--- at_t/src/main/java/homepage/at_tHomePage.java
+++ at_t/src/main/java/homepage/at_tHomePage.java
@@ -1,30 +1,46 @@
 package homepage;
 
 import common.WebAPI;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.Select;
 
-import static webelements.at_tWebelements.searchbox;
+import static webelements.at_tWebelements.*;
 
 //first extend the WebAPI
 public class at_tHomePage extends WebAPI {
     // using Findbyannotation which is under selinium
     // must import Webelements
 
+
     @FindBy(how = How.CSS, using = searchbox )
+    WebElement usersearchbox;
+    @FindBy(how = How.CSS, using = clicksearchbutton )
+    WebElement clickonsearchbutton;
+    @FindBy(how = How.CSS, using = accountlogin)
+    WebElement useraccountlogin;
+    @FindBy(how = How.XPATH, using = clickimageiphone)
+    WebElement webelementofiphoneimage;
+    @FindBy(how = How.CSS, using = pricingoptions)
+    WebElement webElementpricingoptions;
+
+
 
     //initialize and import webelements
 
-    public WebElement searchBoxwebelement;
 
     // created a method for searchbox
     public void usersearchbox(){
-       driver.get("https://www.att.com/");
-        searchBoxwebelement.sendKeys("AT&T deals");
-
+       driver.get(url);
+        usersearchbox.sendKeys("AT&T deals");
     }
+    public void searchclickbutton(){
+        clickonsearchbutton.click();
+    }
+
     //Scrolldownwebpage
     public void scrolldownwebpage(){
         JavascriptExecutor scroll = (JavascriptExecutor)driver;
@@ -35,8 +51,26 @@ public class at_tHomePage extends WebAPI {
     public void scrolltoend() throws InterruptedException {
         JavascriptExecutor scrollend = (JavascriptExecutor)driver;
         scrollend.executeScript("window.scrollTo(10, document.body.scrollHeight");
-//        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    //  driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         Thread.sleep(2000);
+
+    }
+    // accountlog in
+    public void clickaccountlogin() {
+        useraccountlogin.click();
+
+    }
+    //clickoniphoneimage
+    public void clickoniphoneimage() {
+        webelementofiphoneimage.click();
+
+    }
+    //iphonepricing option
+    public void selectpricingoption(){
+        driver.get("https://www.att.com/buy/phones/apple-iphone-11-64gb-purple.html?offerid=13700004");
+        webElementpricingoptions.click();
+        Select prcoption = new Select(driver.findElement(By.xpath(pricingoptions)));
+        prcoption.selectByIndex(1);
 
     }
 
--- at_t/src/test/java/hometest/at_tHomeTest.java
+++ at_t/src/test/java/hometest/at_tHomeTest.java
@@ -10,31 +10,71 @@ public class at_tHomeTest extends WebAPI {
     static at_tHomePage mainhomepage;
     //webelement initialization
 
+    //create page factory class ad driver and call the at_thomePage
     public void getInitElements() {
         mainhomepage = PageFactory.initElements(driver, at_tHomePage.class);
     }
-//     to count test case
-    @Test(priority = 1)
-    public void testusersearchbox() throws InterruptedException {
-        driver.manage().window().maximize();
-        getInitElements();
-        mainhomepage.usersearchbox();
-        Thread.sleep(5000);
-    }
 
-    @Test (priority = 2)
-    public void testscroll() throws InterruptedException {
-        driver.manage().window().maximize();
-        getInitElements();
-        mainhomepage.scrolldownwebpage();
-        Thread.sleep(5000);
-    }
-    @Test (priority = 3)
-    public void testscrolltoend() throws InterruptedException {
-        driver.get("https://www.att.com/");
-        driver.manage().window().maximize();
-        getInitElements();
-        mainhomepage.scrolltoend();
-    }
+
+
+    // to count test case
+//    @Test(priority = 1)
+//    public void testusersearchbox() throws InterruptedException {
+//        driver.manage().window().maximize();
+//        getInitElements();
+//        mainhomepage.usersearchbox();
+//        Thread.sleep(5000);
+//    }
+//
+//    @Test(priority = 2)
+//    public void testsearchclickbutton() throws InterruptedException {
+//        driver.manage().window().maximize();
+//        getInitElements();
+//        mainhomepage.usersearchbox();
+//        mainhomepage.searchclickbutton();
+//        Thread.sleep(3000);
+//    }
+//
+//    @Test (priority = 3)
+//    public void testscroll() throws InterruptedException {
+//        driver.manage().window().maximize();
+//        getInitElements();
+//        mainhomepage.scrolldownwebpage();
+//        Thread.sleep(5000);
+//    }
+//    @Test (priority = 4)
+//    public void testscrolltoend() throws InterruptedException {
+//        driver.get("https://www.att.com/");
+//        driver.manage().window().maximize();
+//        getInitElements();
+//        mainhomepage.scrolltoend();
+//    }
+//
+//    @Test(priority = 5)
+//    public void testclickaccountlogin(){
+//        driver.manage().window().maximize();
+//        getInitElements();
+//        mainhomepage.clickaccountlogin();
+//    }
+
+//      @Test(priority = 6)
+//      public void testiphoneimage() throws InterruptedException {
+//          driver.manage().window().maximize();
+//          getInitElements();
+//          mainhomepage.clickoniphoneimage();
+//          Thread.sleep(3000);
+//
+//      }
+
+      @Test(priority = 7)// not working
+    public void testpricingoption() throws InterruptedException {
+          driver.get("https://www.att.com/buy/phones/apple-iphone-11-64gb-purple.html?offerid=13700004");
+          driver.manage().window().maximize();
+          getInitElements();
+//          mainhomepage.clickoniphoneimage();
+          mainhomepage.selectpricingoption();
+          Thread.sleep(3000);
+
+      }
 
 }
