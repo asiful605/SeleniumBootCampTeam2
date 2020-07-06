be89e007b85685a0289f1e24ceabd50912e4a373
abidur1999 <abidur1999@gmail.com>
2020-07-05 22:26:26 -0400

    updateexpidia

--- Expedia/src/main/java/Homepage/ExpediaHomeTest.java
+++ Expedia/src/main/java/Homepage/ExpediaHomeTest.java
@@ -1,4 +1,76 @@
 package Homepage;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import common.WebAPI;
 
-public class ExpediaHomeTest {
+public class ExpediaHomeTest extends WebAPI {
+    @FindBy(how= How.CSS,using=clickElement)
+    WebElement clickOnButton;
+
+    @FindBy(how= How.CSS,using=accountTab)
+    WebElement accountTabelement;
+
+
+    @FindBy(how= How.CSS,using=checkbox)
+    WebElement checkboxabelement;
+
+
+    @FindBy(how= How.XPATH,using=imageElement)
+    WebElement imageWebelement;
+
+
+    @FindBy(how= How.XPATH,using= checkboxelement)
+    WebElement  usercheckboxelement;
+
+
+
+
+
+
+    public void userclickOnButton(){
+        driver.get( url);
+        clickOnButton.click();
+    }
+    public void clickonAccountTab(){
+        driver.get( url2);
+        driver.manage().window().maximize();
+        accountTabelement.click();
+        System.out.println( driver.getTitle());
+    }
+    public void clickOnCheckBox(){
+        driver.get( url2);
+        driver.manage().window().maximize();
+        checkboxabelement.isSelected();
+        System.out.println( driver.getTitle());
+    }
+    public void clickOnimageelement(){
+        driver.get( url2);
+        driver.manage().window().maximize();
+        imageWebelement.click();
+        System.out.println( driver.getTitle());
+    }
+
+    public void scrollDownTheWebPage() throws InterruptedException {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,1000)");
+        Thread.sleep(3000);
+    }
+
+
+    public void scrollDownTillEnd() throws InterruptedException {
+        //This will scroll the web page till end.
+        JavascriptExecutor js1 = (JavascriptExecutor) driver;
+        js1.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        Thread.sleep(3000);
+    }
+
+
+    public void clickOncheckBox() throws InterruptedException {
+        usercheckboxelement.isSelected();
+        Thread.sleep(3000);
+
+
+    }
 }
--- Expedia/src/test/java/hometest/ExpediaHomeTest.java
+++ Expedia/src/test/java/hometest/ExpediaHomeTest.java
@@ -1,4 +1,57 @@
 package hometest;
 
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
 public class ExpediaHomeTest {
+    static ExpediaHomeTest homeTest ;// Reference variable/object of CignaHomePage
+
+    public static void getInitElements(){
+        ExpediaHomeTest= PageFactory.initElements(driver, homeTest.class);
+    }
+
+@Test(priority  =1)
+  public void testuserclickOnButton() {
+        getInitElements();
+    HomePage.userclickOnButton();
+}
+
+ @Test(priority  =2)
+    public void testclickonAccountTab(){
+       getInitElements();
+       HomePage.clickonAccountTab();
+    }
+
+    @Test(priority  =3)
+    public void testclickonclickOnCheckBox() throws InterruptedException {
+        getInitElements();
+        HomePage.clickOnCheckBox();
+        Thread.sleep(3000);
+    }
+@Test(priority  =4)
+    public void testclickOnimageelement(){
+       getInitElements();
+       HomePage.clickOnimageelement();
+    }
+
+@Test(priority  =5)
+    public void scrollDownTheWebPage() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,1000)");
+    }
+
+
+    @Test(priority  =6)
+    public void testscrollDownTillEnd() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,1000)");
+    }
+
+
+    @Test(priority  =6)
+   public void testclickOncheckBox() throws InterruptedException{
+     getInitElements();
+     HomePage.clickOncheckBox();
+    }
+
 }
